fb0622d04541febc7b922d22a04b3672d49b2c0b
Travis Gandy <travis_gandy@yahoo.com>
2020-04-30 21:53:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,8 +27,8 @@ class Song
       new_song
   end 
   
-  def self.find_by_name 
-      self.all.find{|person| person.name == name}
+  def self.find_by_name(song_name)
+      self.all.find{|songs| songs.name == song_name}
   end 
 
 end

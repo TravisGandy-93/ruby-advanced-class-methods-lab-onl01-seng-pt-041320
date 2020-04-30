1eb89a41a9f19cb33f5b4574ec5dfb0ee1693943
Travis Gandy <travis_gandy@yahoo.com>
2020-04-30 22:41:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,7 +40,7 @@ class Song
   end 
   
   def self.new_from_filename(filename)
-      file = filename.split("-")
+      file = filename.split("- ")
       new_song = new_by_name(file[1].chomp(".mp3"))
       new_song.artist_name = file[0]
       new_song

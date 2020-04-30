4102c8694850cdfa140feb57e49433bef9d00926
Travis Gandy <travis_gandy@yahoo.com>
2020-04-30 21:43:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,19 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create
+      self.all << Song.new 
+      self.all.last 
+  end 
+  
+  def self.new_by_name(song_title)
+      song = Song.new
+      song.name = song_title
+      song 
+  end 
+  
+  def self.create_by_name(song)
+      new_song = Song.create
 
 end
